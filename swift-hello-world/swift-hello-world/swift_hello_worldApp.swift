620214999684702bfd6ad1d60b5921817320c49d
Narendra Yenugula <narendra@beyondsports.nl>
2024-10-07 22:08:46 +0200

    Added hello world project

---
+++ swift-hello-world/swift-hello-world/swift_hello_worldApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_hello_worldApp.swift
+//  swift-hello-world
+//
+//  Created by Narendra Yenugula on 07/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct swift_hello_worldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ swift-hello-world/swift-hello-worldTests/swift_hello_worldTests.swift
@@ -0,0 +1,16 @@
+//
+//  swift_hello_worldTests.swift
+//  swift-hello-worldTests
+//
+//  Created by Narendra Yenugula on 07/10/2024.
+//
+
+import Testing
+
+struct swift_hello_worldTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
