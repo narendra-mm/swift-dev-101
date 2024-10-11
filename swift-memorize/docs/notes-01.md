cdd53777fab8f0d34458ef583a0feecde3792b2a
Narendra Yenugula <narendra@beyondsports.nl>
2024-10-11 20:58:29 +0200

    Added new project that follows Standford CS193p

---
+++ swift-memorize/docs/notes-01.md
@@ -0,0 +1,8 @@
+# Xcode
+Defualt IDE to learn swift to program for Apple multiplatform devices
+
+#Swift
+Base language to use to write apps for Apple devices
+
+#SwiftUI
+A UI framework to create UIs that follow functional programming.
---
+++ swift-memorize/swift-memorize/swift_memorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  swift_memorizeApp.swift
+//  swift-memorize
+//
+//  Created by Narendra Yenugula on 11/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct swift_memorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
